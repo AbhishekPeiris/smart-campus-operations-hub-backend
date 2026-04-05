e5ee77a63e7bffa08bc7c4cc6640eb0f5b5fa1d7
Abhishek Peiris <abhisheklpeiris@gmail.com>
2026-04-05 19:42:15 +0530

    services

--- operationshub/src/main/java/com/smartcampus/operationshub/auth/service/AuthenticationService.java
+++ operationshub/src/main/java/com/smartcampus/operationshub/auth/service/AuthenticationService.java
@@ -1,5 +1,18 @@
 package com.smartcampus.operationshub.auth.service;
 
-public class AuthenticationService {
-    
+import com.smartcampus.operationshub.auth.dto.LoginRequest;
+import com.smartcampus.operationshub.auth.dto.LoginResponse;
+import com.smartcampus.operationshub.auth.dto.RegisterUserRequest;
+
+public interface AuthenticationService {
+
+    /**
+     * Register a new user
+     */
+    void registerUser(RegisterUserRequest request);
+
+    /**
+     * Login user and return JWT response
+     */
+    LoginResponse login(LoginRequest request);
 }
--- operationshub/src/main/java/com/smartcampus/operationshub/ticket/service/TicketAttachmentService.java
+++ operationshub/src/main/java/com/smartcampus/operationshub/ticket/service/TicketAttachmentService.java
@@ -1,5 +1,23 @@
 package com.smartcampus.operationshub.ticket.service;
 
-public class TicketAttachmentService {
-    
+import java.util.List;
+
+import com.smartcampus.operationshub.ticket.dto.response.TicketAttachmentResponse;
+
+public interface TicketAttachmentService {
+
+    /**
+     * Upload attachment to ticket
+     */
+    TicketAttachmentResponse uploadAttachment(String ticketId, String fileName, String fileType, String fileUrl, String uploadedBy);
+
+    /**
+     * Get all attachments for a ticket
+     */
+    List<TicketAttachmentResponse> getAttachmentsByTicket(String ticketId);
+
+    /**
+     * Delete attachment
+     */
+    void deleteAttachment(String attachmentId);
 }
