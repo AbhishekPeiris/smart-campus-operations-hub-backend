50bd35236072e276177cb9c58c6c966774039f3d
Abhishek Peiris <abhisheklpeiris@gmail.com>
2026-04-05 19:26:23 +0530

    repo

--- operationshub/src/main/java/com/smartcampus/operationshub/ticket/repository/TicketAttachmentRepository.java
+++ operationshub/src/main/java/com/smartcampus/operationshub/ticket/repository/TicketAttachmentRepository.java
@@ -1,5 +1,18 @@
 package com.smartcampus.operationshub.ticket.repository;
 
-public class TicketAttachmentRepository {
-    
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.smartcampus.operationshub.ticket.model.TicketAttachment;
+
+@Repository
+public interface TicketAttachmentRepository extends MongoRepository<TicketAttachment, String> {
+
+    List<TicketAttachment> findByTicketId(String ticketId);
+
+    long countByTicketId(String ticketId);
+
+    void deleteByTicketId(String ticketId);
 }
